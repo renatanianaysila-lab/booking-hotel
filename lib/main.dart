d554c8bf45be78bd83f421fac99d028954e09034
aurelzlsa <aurelzalsabillaaa@gmail.com>
2026-05-07 22:19:47 +0700

    Update splash screen2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
+import 'screens/splash_screen2.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'splash_screen2.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SplashScreen2()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +33,7 @@ class SplashScreen extends StatelessWidget {
                 'assets/Logo.png',
                 width: 120,
               ),
-
               const SizedBox(height: 24),
-
               const Text(
                 'Pesan Hotel',
                 style: TextStyle(
