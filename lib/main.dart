52b6e09f33c0ec1a12b9bee559ceee6d1b158859
Naysila Renatania <renatanianaysila@gmail.com>
2026-05-09 11:44:50 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,19 +10,18 @@ void main() async {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwc2hkZnFqcWl5a2N6ZnFqdWxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNzE5NTgsImV4cCI6MjA5Mzg0Nzk1OH0.PqLDl4epeY-uF1s1jSGM2yKbnlnW6lcy3tcgZn3yQnw',
   );
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Booking Hotel',
-      home: Scaffold(
-        body: Center(
-          child: Text('Supabase Connected!'),
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
+
